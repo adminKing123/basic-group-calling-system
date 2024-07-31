099a059680135ef90644313101175ff1f1558ad5
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-07-31 16:47:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files from the 'public' directory
+app.use(express.static("public"));
+
+// Endpoint to serve index.html
+app.get("/:room/connect", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("New user connected:", socket.id);
+
+  socket.on("join", (room) => {
+    socket.join(room);
+    socket.to(room).emit("user-connected", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.to(room).emit("user-disconnected", socket.id);
+    });
+
+    socket.on("send-signal", ({ signal, userId }) => {
+      io.to(userId).emit("receive-signal", { signal, userId: socket.id });
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
